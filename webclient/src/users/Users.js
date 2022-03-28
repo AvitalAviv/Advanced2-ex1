4cc552e59fe687d0cd2bb28d30977bec631da869
Omer Aplatony <omerap12@gmail.com>
2022-03-28 19:12:32 +0300

    push

---
+++ webclient/src/users/Users.js
@@ -0,0 +1,4 @@
+const Users = [{name: "Omer",password:"12345678Aa"},{name:"Avital",password:"87654321Aa"}]
+
+
+export default Users;
---
+++ webclient/src/validation/Validation.js
@@ -0,0 +1,11 @@
+function Validation({ user }) {
+    var user_password = user.password;
+    var user_username = user.username;
+    
+
+
+
+
+
+    return;
+}
