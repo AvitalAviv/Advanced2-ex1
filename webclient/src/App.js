30bca9f2f2639b8020e3f8a53a97f50ec1806fb7
Omer Aplatony <omerap12@gmail.com>
2022-03-29 16:54:19 +0300

    Alert

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -1,22 +1,30 @@
 import './App.css';
 import logo from './logo.png';
-import users from './users/CheckUser.js'
+import CheckUser from './users/CheckUser.js'
+import AlertWindow from './alertwindow/AlertWindow.js'
 
 
 function App() {
-  const demo = (event) => {
-    event.preventDefault();
+  const dbChecker = (event) => {
+    event.preventDefault(); //prevent refresh
+
     var userName = document.getElementById("userName").value;
     var userPassword = document.getElementById("pwd").value;
-    var userChecker = users;
-    console.log(userChecker(userName, userPassword));
+    document.getElementById('userName').value = ''
+    document.getElementById('pwd').value = ''
+
+    var doesOk = CheckUser(userName, userPassword);
+    if (doesOk != true) {
+      AlertWindow();
+    }
+    //need to complete whether user can enter the site
   };
 
   return (
     <div className="wrapper">
       <div className="logo"> <img src={logo} alt=""></img> </div>
       <div className="text-center mt-4 name"> TalkToMe </div>
-      <form className="p-3 mt-3" onSubmit={demo}>
+      <form className="p-3 mt-3" onSubmit={dbChecker}>
         <div className="form-field d-flex align-items-center"> <span className="far fa-user"></span>
           <input type="text" name="userName" id="userName" placeholder="Username"></input></div>
         <div className="form-field d-flex align-items-center"> <span className="fas fa-key"></span>
---
+++ webclient/src/alertwindow/AlertWindow.js
@@ -0,0 +1,4 @@
+function AlertWindow() {
+    alert("User name or Password incorrect.");
+}
+export default AlertWindow;
