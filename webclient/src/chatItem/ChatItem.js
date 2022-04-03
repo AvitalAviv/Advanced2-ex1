eb829630a56d1bd26207036c3a34561411cd6b94
Avital Aviv <avitalos6@gmail.com>
2022-04-03 14:41:19 +0300

    a

--- webclient/src/chatItem/ChatItem.js
+++ webclient/src/chatItem/ChatItem.js
@@ -2,12 +2,7 @@ import "./ChatItem.css";
 
 function ChatItem({ nick_name, user_image, last_message, last_modified }) {
     return(
-        <div className="chatItem">
-            <img src={user_image} > </img>
-            <div>nick_name</div>
-            <div>time</div>
-            <div>last message</div>
-        </div>
+
     );
 }
 
--- webclient/src/chatRoom/ChatRoom.js
+++ webclient/src/chatRoom/ChatRoom.js
@@ -6,8 +6,14 @@ function ChatRoom() {
     return (
         <div className="chatRoom">
             <div className="chatRoom_body">
-                <SideBar/>
-                {/*body*/}
+               <div className="sideBar">
+                    <div className="chatItem">
+                        <img src={user_image} > </img>
+                        <div>nick_name</div>
+                        <div>time</div>
+                        <div>last message</div>
+                    </div>
+               </div>
             </div>
         </div>
     )
