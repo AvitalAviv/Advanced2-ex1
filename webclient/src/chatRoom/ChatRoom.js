670861dcd474950af80c5be33d4a5bef0f9f55e6
Omer Aplatony <omerap12@gmail.com>
2022-04-12 12:31:55 +0300

    fixed

--- webclient/src/chatRoom/ChatRoom.js
+++ webclient/src/chatRoom/ChatRoom.js
@@ -6,14 +6,13 @@ import RightSideChat from '../rightSideChat/RightSideChat';
 
 function ChatRoom(props) {
     const [selected, setSelected] = React.useState("null");
-    
+
     if (selected === "null") {
         return (
             <div className="container-fluid d-flex flex-column background-page" style={{ width: "100%", height: "100%", position: "absolute", blockSize: "cover" }}>
                 <div className="card" style={{ width: "80%", height: "90%", position: "absolute", backgroundColor: "white", margin: "5.8%", marginTop: "2.3%", alignSelf: "center" }}>
                     <div className="row" style={{ background: "blue", blockSize: "cover", marginLeft: "-0.01%", height: "100%", width: "100%" }}>
                         <LeftSideChat User={props.User} DbUsers={props.DbUsers} setSelected={setSelected} />
-                        <RightSideChat User={props.User} DbUsers={props.DbUsers} setSelected={setSelected} />
                     </div>
                 </div>
             </div>
@@ -25,7 +24,7 @@ function ChatRoom(props) {
                 <div className="card" style={{ width: "80%", height: "90%", position: "absolute", backgroundColor: "white", margin: "5.8%", marginTop: "2.3%", alignSelf: "center" }}>
                     <div className="row" style={{ background: "blue", blockSize: "cover", marginLeft: "-0.01%", height: "100%", width: "100%" }}>
                         <LeftSideChat User={props.User} DbUsers={props.DbUsers} setSelected={setSelected} />
-                        <RightSideChat User={props.User} DbUsers={props.DbUsers} setSelected={setSelected} />
+                        <RightSideChat User={props.User} DbUsers={props.DbUsers} setSelected={setSelected} selected={selected} />
                     </div>
                 </div>
             </div>
