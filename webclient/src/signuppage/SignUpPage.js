78e811959615c6c4bdabccebc466064254a2350d
Avital Aviv <avitalos6@gmail.com>
2022-04-14 19:12:47 +0300

    a

--- webclient/src/signuppage/SignUpPage.js
+++ webclient/src/signuppage/SignUpPage.js
@@ -61,31 +61,6 @@ class SignUp extends Component {
       return (<SignInPage DbUsers={this.dbUsers} />);
     }
     return (
-      // <div className="wrapper-register-page" >
-      //   <div className="logo"> <img src={logo} alt=""></img> </div>
-      //   <div className="text-center mt-4 name" id='Headline'> Sign up </div>
-      //   <form className="p-3 mt-3">
-      //     <div className="form-field d-flex align-items-center"> <span className="far fa-user"></span>
-      //       <input type="text" name="userName" id="userName" placeholder="User name"></input></div>
-      //     <div className="form-field d-flex align-items-center"> <span className="fas fa-key"></span>
-      //       <input type="text" name="nickName" id="nickName" placeholder="Display name"></input></div>
-      //     <div className="form-field d-flex align-items-center"> <span className="far fa-user"></span>
-      //       <input type="password" name="password1" id="password1" placeholder="password"></input></div>
-      //     <div className="form-field d-flex align-items-center"> <span className="far fa-user"></span>
-      //       <input type="password" name="password2" id="password2" placeholder="repeat password"></input></div>
-      //     <div>choose your profile image</div>
-
-      //     <label class="file"><input type="file" onChange={(event) => {
-      //       this.image_url = URL.createObjectURL(event.target.files[0]);
-      //     }} id="image-user-input" class="form-control-file" accept="image/*"></input>
-      //       <span class="file-custom"></span></label>
-      //     <br></br>
-      //     <br></br>
-      //     <button class="btn mt-3" onClick={handleRegister}>Register</button>
-      //   </form>
-      //   <br></br>
-      //   <div className="text-center fs-6"> Already registred? <a href="/#" onClick={handleClickHere}>Click here</a> to login</div>
-      // </div >
       <div className="wrapper wrapper-register-page card" style={{marginTop: "0.3%", marginBottom: "0%"}}>
         <div className='login-side'>
           <div className="logo"> <img src={logo} alt=""></img> </div>
