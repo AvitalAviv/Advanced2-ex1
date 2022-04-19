41424bdb984218a811b66cb2ca2d8a6d0674cf0c
Omer Aplatony <omerap12@gmail.com>
2022-04-19 19:04:23 +0300

    Updated db

--- webclient/src/users/DbUsers.js
+++ webclient/src/users/DbUsers.js
@@ -20,33 +20,33 @@ class DbUsers {
         this.db = new Map();
         this.db.set("Omer_Apla", new User("Omer_Apla", "scrum master", "12345678Aa", omerphoto));
         this.db.set("Avital_os", new User("Avital_os", "Avitalos", "87654321Aa", avitalphoto));
-        this.db.set("Ron", new User("Ron", "Ronen", "123", ronphoto));
-        this.db.set("Niv", new User("Niv", "NivNah", "123", nivphoto));
-        this.db.set("Dvir", new User("Dvir", "Dviri", "123", dvitphoto));
-        this.db.set("David", new User("David", "King", "123", davidphoto));
+        this.db.set("Ron_banini", new User("Ron_banini", "Ronen_even", "12345678Aa", ronphoto));
+        this.db.set("NivNahman", new User("NivNahman", "NivNah", "12345678Aa", nivphoto));
+        this.db.set("Dvir_amram", new User("Dvir_amram", "Dviri", "12345678Aa", dvitphoto));
+        this.db.set("David_IS", new User("David_IS", "KingDavid", "12345678Aa", davidphoto));
 
         this.user_nickname_db = new Map();
-        this.user_nickname_db.set("Omer", "scrum master");
-        this.user_nickname_db.set("Ron", "Ronen");
-        this.user_nickname_db.set("Niv", "NivNah");
-        this.user_nickname_db.set("Dvir", "Dviri");
-        this.user_nickname_db.set("David", "King");
-        this.user_nickname_db.set("Avitalos", "Avitalos");
+        this.user_nickname_db.set("Omer_Apla", "scrum master");
+        this.user_nickname_db.set("Ron_banini", "Ronen_even");
+        this.user_nickname_db.set("NivNahman", "NivNah12");
+        this.user_nickname_db.set("Dvir_amram", "Dviri");
+        this.user_nickname_db.set("David_IS", "KingDavid");
+        this.user_nickname_db.set("Avital_os", "Avitalos");
 
 
         //creating chats between Omer to all parties
         var chat_one = new Chat("scrum master", "Avitalos", omerphoto, avitalphoto);
-        var chat_two = new Chat("scrum master", "Ronen", omerphoto, ronphoto);
-        var chat_three = new Chat("scrum master", "NivNah", omerphoto, nivphoto);
+        var chat_two = new Chat("scrum master", "Ronen_even", omerphoto, ronphoto);
+        var chat_three = new Chat("scrum master", "NivNah12", omerphoto, nivphoto);
         var chat_four = new Chat("Omer_Apla", "Dviri", omerphoto, dvitphoto);
-        var chat_five = new Chat("Omer_Apla", "King", omerphoto, davidphoto);
+        var chat_five = new Chat("Omer_Apla", "KingDavid", omerphoto, davidphoto);
 
         //add messages to chat
         chat_one.addMessage(new Message("Avitalos", "text", "Naama!", time_now));
-        chat_two.addMessage(new Message("Ronen", "audio", "path", time_now));
-        chat_three.addMessage(new Message("NivNah", "image", nivphoto, time_now));
+        chat_two.addMessage(new Message("Ronen_even", "audio", "path", time_now));
+        chat_three.addMessage(new Message("NivNah12", "image", nivphoto, time_now));
         chat_four.addMessage(new Message("Dviri", "video", mario, time_now));
-        chat_five.addMessage(new Message("King", "text", "How are you? Maybe well go to drink coffe sometimes", time_now));
+        chat_five.addMessage(new Message("KingDavid", "text", "How are you? Maybe well go to drink coffe sometimes", time_now));
 
         //add chats to user
         this.db.get("Omer_Apla").chats.push(chat_one);
@@ -55,9 +55,9 @@ class DbUsers {
         this.db.get("Omer_Apla").chats.push(chat_four);
         this.db.get("Omer_Apla").chats.push(chat_five);
 
-        this.db.get("Niv").chats.push(chat_three);
-        this.db.get("Dvir").chats.push(chat_four);
-        this.db.get("David").chats.push(chat_five);
+        this.db.get("NivNahman").chats.push(chat_three);
+        this.db.get("Dvir_amram").chats.push(chat_four);
+        this.db.get("David_IS").chats.push(chat_five);
     }
 
     //need to check again   
