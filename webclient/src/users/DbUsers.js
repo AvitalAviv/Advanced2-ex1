d8ca7982042b382d822892463adf488652a60a49
Avital Aviv <avitalos6@gmail.com>
2022-04-19 20:11:12 +0300

    a

--- webclient/src/users/DbUsers.js
+++ webclient/src/users/DbUsers.js
@@ -22,14 +22,14 @@ class DbUsers {
         this.db.set("Avital_os", new User("Avital_os", "Avitalos", "87654321Aa", avitalphoto));
         this.db.set("Ron_banini", new User("Ron_banini", "Ronen_even", "12345678Aa", ronphoto));
         this.db.set("NivNahman", new User("NivNahman", "NivNah", "12345678Aa", nivphoto));
-        this.db.set("Dvir_amram", new User("Dvir_amram", "Dviri", "12345678Aa", dvitphoto));
+        this.db.set("Dvir_amram", new User("Dvir_amram", "Dviri8080", "12345678Aa", dvitphoto));
         this.db.set("David_IS", new User("David_IS", "KingDavid", "12345678Aa", davidphoto));
 
         this.user_nickname_db = new Map();
         this.user_nickname_db.set("Omer_Apla", "scrum master");
         this.user_nickname_db.set("Ron_banini", "Ronen_even");
         this.user_nickname_db.set("NivNahman", "NivNah12");
-        this.user_nickname_db.set("Dvir_amram", "Dviri");
+        this.user_nickname_db.set("Dvir_amram", "Dviri8080");
         this.user_nickname_db.set("David_IS", "KingDavid");
         this.user_nickname_db.set("Avital_os", "Avitalos");
 
@@ -38,14 +38,14 @@ class DbUsers {
         var chat_one = new Chat("scrum master", "Avitalos", omerphoto, avitalphoto);
         var chat_two = new Chat("scrum master", "Ronen_even", omerphoto, ronphoto);
         var chat_three = new Chat("scrum master", "NivNah12", omerphoto, nivphoto);
-        var chat_four = new Chat("Omer_Apla", "Dviri", omerphoto, dvitphoto);
+        var chat_four = new Chat("Omer_Apla", "Dviri8080", omerphoto, dvitphoto);
         var chat_five = new Chat("Omer_Apla", "KingDavid", omerphoto, davidphoto);
 
         //add messages to chat
         chat_one.addMessage(new Message("Avitalos", "text", "Naama!", time_now));
         chat_two.addMessage(new Message("Ronen_even", "audio", "path", time_now));
         chat_three.addMessage(new Message("NivNah12", "image", nivphoto, time_now));
-        chat_four.addMessage(new Message("Dviri", "video", mario, time_now));
+        chat_four.addMessage(new Message("Dviri8080", "video", mario, time_now));
         chat_five.addMessage(new Message("KingDavid", "text", "How are you? Maybe well go to drink coffe sometimes", time_now));
 
         //add chats to user
